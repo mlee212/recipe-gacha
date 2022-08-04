e67b90fe552e93243a8158064b15c0ca9bfc6cc3
mlee212 <mattlee525@gmail.com>
2022-08-03 19:04:23 -0700

    testing env variables #8

--- next.config.js
+++ next.config.js
@@ -21,6 +21,6 @@ module.exports = {
     
       apiUrl: process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000/api' // development api
-          : ('https//' + process.env.NEXT_PUBLIC_VERCEL_URL + '/api') // production api
+          : ('https://' + process.env.NEXT_PUBLIC_VERCEL_URL + '/api') // production api
   }
 }
