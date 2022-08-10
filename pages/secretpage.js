f95f5d98dcfe1952a9c9fd58a3c8d4e9606759fb
mlee212 <mattlee525@gmail.com>
2022-08-10 00:23:26 -0700

    Removed embed attempt

--- pages/secretpage.js
+++ pages/secretpage.js
@@ -9,8 +9,7 @@ export default function SecretPage() {
             </div>
             <div>
                 unless u wanna play games? 
-                <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=10,0,0,0" id="gameObject" width="700" height="500"><param name="movie" value="/newGames/strategy-games/infectionator/infectionator_w.swf"/><param name="menu" value="false"/><param name="allowscriptaccess" value="samedomain"/><param name="allownetworking" value="all"/><embed src="/newGames/strategy-games/infectionator/infectionator_w.swf" width="700" height="500" menu="false"  allowscriptaccess="samedomain" allownetworking="all" name="gameObject" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object><span style="font-size:11px;"><br/><a href="" style="font-size:11px;">Games</a>:<a href="http://www.addictinggames.com/strategy-games/index.jsp" style="font-size:11px;">Play Strategy Games on AddictingGames</a><br/><a href="http://www.addictinggames.com/legal/terms-use.jsp" style="font-size:11px;">Terms of Use</a> | <a href="http://www.addictinggames.com/legal/copyright.jsp" style="font-size:11px;">&copy; 2012 Atom Entertainment, Inc., a subsidiary of Viacom International Inc. All Rights Reserved.</a></span>
-
+                
             </div>
         </>
     )
